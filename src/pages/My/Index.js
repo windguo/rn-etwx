b0cc6e7d8bb42882d2f68db5617beb5bdf9aa824
windguo <webmaster@windguo.com>
2018-05-24 08:25:16 +0800

    2

--- src/pages/My/Index.js
+++ src/pages/My/Index.js
@@ -243,40 +243,6 @@ export default class Me extends Component {
         return (
             <ScrollView style={{ flex: 1, backgroundColor: Color.f5f5f5 }}>
                 <View style={{ width: WIDTH, height: 10, backgroundColor: Color.f5f5f5 }} />
-                <View style={{ height: 1, backgroundColor: Color.f5f5f5 }}></View>
-                <TouchableOpacity activeOpacity={1} onPress={this.clickToLogin}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', height: 50, backgroundColor: 'white', justifyContent: 'space-between' }}>
-                        <View style={{ marginLeft: 20, flexDirection: 'row', alignItems: 'center' }}>
-                            <IconSimple name="user" size={22} color={Color.FontColor} />
-                            <Text style={{ marginLeft: 10 }}>{this.state.username ? '欢迎您,' + this.state.username : '立即登录'}</Text>
-                        </View>
-                        <IconSimple name="arrow-right" size={18} color={Color.FontColor} style={{ marginRight: 20 }} />
-                    </View>
-                </TouchableOpacity>
-                <View style={{ height: 1, backgroundColor: Color.f5f5f5 }}></View>
-                {this.state.username ?
-                    <View>
-                        <TouchableOpacity activeOpacity={1} onPress={this.clickToPublish}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', height: 50, backgroundColor: 'white', justifyContent: 'space-between' }}>
-                                <View style={{ marginLeft: 20, flexDirection: 'row', alignItems: 'center' }}>
-                                    <IconSimple name="wallet" size={22} color={Color.FontColor} />
-                                    <Text style={{ marginLeft: 10 }}>我发布的内容</Text>
-                                </View>
-                                <IconSimple name="arrow-right" size={18} color={Color.FontColor} style={{ marginRight: 20 }} />
-                            </View>
-                        </TouchableOpacity>
-                        <View style={{ height: 1, backgroundColor: Color.f5f5f5 }}></View>
-                        {/* <TouchableOpacity activeOpacity={1} onPress={this.clickToCollection}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', height: 50, backgroundColor: 'white', justifyContent: 'space-between' }}>
-                                <View style={{ marginLeft: 20, flexDirection: 'row', alignItems: 'center' }}>
-                                    <IconSimple name="wallet" size={22} color={Color.FontColor} />
-                                    <Text style={{ marginLeft: 10 }}>我收藏的内容</Text>
-                                </View>
-                                <IconSimple name="arrow-right" size={18} color={Color.FontColor} style={{ marginRight: 20 }} />
-                            </View>
-                        </TouchableOpacity> */}
-                    </View> : <View />}
-                <View style={{ width: WIDTH, height: 10, backgroundColor: Color.f5f5f5 }} />
                 <TouchableOpacity activeOpacity={1} onPress={() => { this.pushToWeb('yjfk') }}>
                     <View style={{ flexDirection: 'row', alignItems: 'center', height: 50, backgroundColor: 'white', justifyContent: 'space-between' }}>
                         <View style={{ marginLeft: 20, flexDirection: 'row', alignItems: 'center' }}>
