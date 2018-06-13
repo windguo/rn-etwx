98c310f50cbc53c4b1cbd2498d059d099443a939
windguo <webmaster@windguo.com>
2018-06-13 14:45:12 +0800

    2037

--- android/app/src/main/java/com/toutiao/MainActivity.java
+++ android/app/src/main/java/com/toutiao/MainActivity.java
@@ -12,6 +12,9 @@ import org.devio.rn.splashscreen.SplashScreen;
 import java.util.Arrays;
 import java.util.List;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -24,6 +27,13 @@ public class MainActivity extends ReactActivity {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+      public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        this.sendBroadcast(intent);
+    }
 
     @Override
     protected String getMainComponentName() {
